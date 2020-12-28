3736cd1576df82a79f6ceb3e42d05e3438aca71c
Vipul Saxena <vipul.saxena01@gmail.comn>
2020-12-29 00:40:02 +0530

    images added to repo. Post wedding revamp done

--- js/main.js
+++ js/main.js
@@ -33,26 +33,12 @@
 	};
 
 	var sliderMain = function() {
-		
-	  	$('#qbootstrap-slider-hero .flexslider').flexslider({
-			animation: "fade",
-			slideshowSpeed: 5000,
-			directionNav: true,
-			start: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			},
-			before: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			}
-
-	  	});
-
+		  $('.flexslider').flexslider({
+		    animation: "fade",
+			slideshowSpeed: 2000,
+			animationSpeed: 4000,
+			smoothHeight: true,
+		  });
 	};
 
 
@@ -251,51 +237,6 @@
 	};
 	
 
-	// Set the date we're counting down to
-		var countDownDate = new Date("Nov 27, 2020 00:00:00").getTime();
-
-		// Update the count down every 1 second
-		var x = setInterval(function() {
-
-		// Get todays date and time
-		var now = new Date().getTime();
-
-		// Find the distance between now an the count down date
-		var distance = countDownDate - now;
-
-		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-		// Display the result in an element with id="demo"
-		// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
-		// + minutes + "Minutes " + seconds + "Seconds ";
-
-		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>days</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
-
-		// If the count down is finished, write some text 
-		if (distance < 0) {
-		 clearInterval(x);
-		 // document.getElementById("demo").innerHTML = "Its a wrap!";
-		 
-		 $("#timer").hide();
-		 $("#go-live").show();
-		 
-		}
-		}, 1000);	
-	
-		
-	var bgVideo = function() {
-		$('.player').mb_YTPlayer();
-	};
-        
-
 	// Document on load.
 	$(function(){
 
@@ -308,7 +249,7 @@
 		navigationSection();
 		contentWayPoint();
 		inlineSVG();
-		bgVideo();
+		// bgVideo();
 	});
 
 
